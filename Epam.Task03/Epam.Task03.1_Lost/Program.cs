bcacbb864ea374eda9e2cc84ffc671299924bdf3
try-inf <try-inf@yandex.ru>
2018-12-10 02:00:47 +0400

    Added Tasks 03.1 and 03.2

---
+++ Epam.Task03/Epam.Task03.1_Lost/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task03._1_Lost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> people = new List<int> { };
+            int qty = QuantityOfPeople();
+            for (int i = 1; i <= qty; i++)
+            {
+                people.Add(i);
+            }
+
+            Console.Write("Current numbers of people in circle: ");
+            ShowList(people);
+            bool flag = false;
+            int count = 0;
+            while (true)
+            {
+                count++;
+                if (people.Count == 1)
+                {
+                    break;
+                }
+                else
+                {
+                    for (int i = 0; i < people.Count; i++)
+                    {
+                        if (flag)
+                        {
+                            people.RemoveAt(i--);
+                        }
+
+                        flag = !flag;
+                    }
+
+                    Console.WriteLine();
+                    Console.Write("After {0} striking out cycle: ", count);
+                    ShowList(people);
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write("Current number of the last person in the circle after striking out: ");
+            ShowList(people);
+
+            Console.WriteLine(Environment.NewLine + "Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static int QuantityOfPeople()
+        {
+            while (true)
+            {
+                Console.Write("Enter quantity of people in a circle: ");
+                string str = Console.ReadLine();
+                bool check = int.TryParse(str, out int result);
+                if (check)
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("You entered not a number");
+                }
+            }
+        }
+
+        private static void ShowList(IEnumerable<int> people)
+        {
+            foreach (var item in people)
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+    }
+}
---
+++ Epam.Task03/Epam.Task03.2_WordFrequency/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task03._2_WordFrequency
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter some text: ");
+            string str = Console.ReadLine();
+            string[] array = str.ToLower().Split(' ', '.');
+            Dictionary<string, List<int>> words = new Dictionary<string, List<int>>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                foreach (var item in array)
+                {
+                    if (words.ContainsKey(item))
+                    {
+                        words[item].Add(1);
+                    }
+                    else
+                    {
+                        words.Add(item, new List<int> { 1 });
+                    }
+                }
+            }
+
+            foreach (var item in words)
+            {
+                int n = item.Value.Count / words.Count;
+                Console.WriteLine("Слово \"{0}\" - встречается в тексте {1} {2}.", item.Key, n, ((n > 1) && (n < 5)) ? "раза" : "раз");
+            }
+
+            Console.WriteLine(Environment.NewLine + "Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
